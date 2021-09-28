68da3c9469460d47f816ae4e1037969b3d4d65a8
sahil9001 <sahilsilare@gmail.com>
2021-09-28 11:23:41 +0530

    static:Staticfile settings

--- djangoproj/settings.py
+++ djangoproj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-wj^+nuw)z$kq#v0jo=v1cf$4k@*-o-nivo&u!7%x1)a@sub^ot
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
--- djangoproj/urls.py
+++ djangoproj/urls.py
@@ -18,11 +18,13 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from posts.views import *
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path("", include("posts.urls")),
 ]
+urlpatterns+=staticfiles_urlpatterns()
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
